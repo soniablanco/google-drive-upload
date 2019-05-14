9fbf9b6508b79d5e8e105645ed3bf9173170136f
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2019-05-14 13:58:41 +0100

    more permission

--- app/app.js
+++ app/app.js
@@ -20,7 +20,7 @@ const readline = require('readline');
 const {google} = require('googleapis');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/drive'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
