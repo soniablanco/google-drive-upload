7ba267c48535615374d0521fc58fc8e12ff24f32
Sonia Carolina Blanco D <soniacarolina.blanco@gmail.com>
2019-05-14 13:08:20 +0100

    using secrets folder

--- app/app.js
+++ app/app.js
@@ -24,10 +24,10 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = '/secrets/token.json';
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile('/secrets/credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
   authorize(JSON.parse(content), listFiles);
